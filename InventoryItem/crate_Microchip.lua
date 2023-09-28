65f34781f6ff78ea42ed724c7395e657feca7825
Alice <alycecil@gmail.com>
2023-09-28 17:41:47 -0400

    cleanup

--- InventoryItem/crate_Microchip.lua
+++ InventoryItem/crate_Microchip.lua
@@ -10,7 +10,7 @@ DefineClass.crate_Microchip = {
 	SubIcon = "UI/Icons/Items/microchip",
 	DisplayName = T(716101335198, --[[ModItemInventoryItemCompositeDef crate_Microchip DisplayName]] "PARTS - Microchip Crate"),
 	DisplayNamePlural = T(716905059151, --[[ModItemInventoryItemCompositeDef crate_Microchip DisplayNamePlural]] "PARTS - Microchip Crates"),
-	Description = T(462227232334, --[[ModItemInventoryItemCompositeDef crate_Microchip Description]] "A crate of 10 generic chips. Use these for crafting."),
-	AdditionalHint = T(732750754205, --[[ModItemInventoryItemCompositeDef crate_Microchip AdditionalHint]] "<image UI/Conversation/T_Dialogue_IconBackgroundCircle.tga 400 130 128 120> Some assembly required."),
+	Description = T(716416655297, --[[ModItemInventoryItemCompositeDef crate_Microchip Description]] "A crate of 10 generic chips. Use these for crafting."),
+	AdditionalHint = T(716844574916, --[[ModItemInventoryItemCompositeDef crate_Microchip AdditionalHint]] "<image UI/Conversation/T_Dialogue_IconBackgroundCircle.tga 400 130 128 120> Some assembly required."),
 }
 
